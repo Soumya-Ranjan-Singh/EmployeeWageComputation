69ad463646a174018c663a33dac30461303a6711
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-10-19 20:57:51 +0530

    Added : Use Case 4 Problem - Solving the previous computation problem using Switch Case Statement

--- src/com/employeewage/computation/EmployeeWageComputation.java
+++ src/com/employeewage/computation/EmployeeWageComputation.java
@@ -2,6 +2,7 @@
 //Use Case-1 is to check whether the Employee is present or not.
 //Use Case-2 is to calculate the daily Employee wage.
 //Use Case-3 is to show part-time Employee's wage.
+//Use Case-4 is to solve that previous program in switch case statement.
 
 package com.employeewage.computation;
 
@@ -19,35 +20,35 @@ public class EmployeeWageComputation {
         System.out.println();
 
         //Declaring the variables.
-        double isPartTime = 1;
-        double isFullTime = 2;
-        double randomCheck = (double) Math.floor((Math.random() * 10) % 3);
+        final int isPartTime = 1;
+        final int isFullTime = 2;
+        int randomCheck = (int) Math.floor((Math.random() * 10) % 3);
         int empHour;
         int wagePerHour = 20;
         int salary = 0;
 
-        //Adding part-time Employee and his/her wage.
-        if (randomCheck == isFullTime)
-        {
-            int fullTimeHour = 8;
-            empHour = fullTimeHour;
-            salary = empHour * wagePerHour;
-            System.out.println("Employee is working full time.");
-            System.out.println("Salary is: $"+salary);
-        }
-        else if (randomCheck == isPartTime)
-        {
-            int partTimeHour = 4;
-            empHour = partTimeHour;
-            salary = empHour * wagePerHour;
-            System.out.println("Employee is working part time.");
-            System.out.println("Salary is: $"+salary);
-        }
-        else
-        {
-            empHour=0;
-            System.out.println("Employee is absent.");
-            System.out.println("Salary is: $"+salary);
+        //Checking for part-time and full time by Switch case.
+        switch (randomCheck) {
+            default :
+                System.out.println("Employee is absent");
+                empHour=0;
+                salary = empHour * wagePerHour;
+                System.out.println("Salary is: $"+salary);
+                break;
+            case isFullTime :
+                System.out.println("Employee is working part-time");
+                int partTimeHour = 4;
+                empHour = partTimeHour;
+                salary = empHour * wagePerHour;
+                System.out.println("Salary is: $"+salary);
+                break;
+            case isPartTime :
+                System.out.println("Employee is working full-time");
+                int fullTimeHour = 8;
+                empHour = fullTimeHour;
+                salary = empHour * wagePerHour;
+                System.out.println("Salary is: $"+salary);
+                break;
         }
 
     }
