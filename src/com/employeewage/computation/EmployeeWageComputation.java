1a75430bce6c3bcbe4d84e21a11caac1283cd1fe
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-10-19 20:05:10 +0530

    Added : Use Case 1 Problem - Check Employee is present or absent

--- src/com/employeewage/computation/EmployeeWageComputation.java
+++ src/com/employeewage/computation/EmployeeWageComputation.java
@@ -1,4 +1,5 @@
 //This program is to display the welcome message to Employee wage computation program.
+//Use Case-1 is to check whether the Employee is present or not.
 
 package com.employeewage.computation;
 
@@ -13,6 +14,21 @@ public class EmployeeWageComputation {
 
         //Initializing the constructor by creating the object of the class.
         EmployeeWageComputation employee = new EmployeeWageComputation();
+        System.out.println();
+
+        //Declaring the variables.
+        double isPresent = 1;
+        double randomCheck = (double)Math.floor((Math.random()*10)%2);
+
+        //Checking if the Employee is present!!!
+        if (randomCheck == isPresent)
+        {
+            System.out.println("Employee is present");
+        }
+        else
+        {
+            System.out.println("Employee is absent");
+        }
 
     }
 }
