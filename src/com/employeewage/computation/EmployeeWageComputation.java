14df18d16e6b8e54aec929cc361107b845f79746
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-10-19 20:14:24 +0530

    Added : Use Case 2 Problem - Calculate daily Employee wage

--- src/com/employeewage/computation/EmployeeWageComputation.java
+++ src/com/employeewage/computation/EmployeeWageComputation.java
@@ -1,5 +1,6 @@
 //This program is to display the welcome message to Employee wage computation program.
 //Use Case-1 is to check whether the Employee is present or not.
+//Use Case-2 is to calculate the daily Employee wage.
 
 package com.employeewage.computation;
 
@@ -18,16 +19,22 @@ public class EmployeeWageComputation {
 
         //Declaring the variables.
         double isPresent = 1;
-        double randomCheck = (double)Math.floor((Math.random()*10)%2);
+        double randomCheck = (double) Math.floor((Math.random() * 10) % 2);
+        int fullDayHour = 8;
+        int wagePerHour = 20;
+        int salary = 0;
 
-        //Checking if the Employee is present!!!
-        if (randomCheck == isPresent)
+        //Checking if the Employee is present, then calculating his/her daily wage.
+        if(randomCheck == isPresent)
         {
             System.out.println("Employee is present");
+            salary = fullDayHour * wagePerHour;
+            System.out.println("Salary is: $"+salary);
         }
         else
         {
             System.out.println("Employee is absent");
+            System.out.println("Salary is: $"+salary);
         }
 
     }
