712cc2ca96595fe0eb062222c2cca836b9100b1e
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-10-19 22:17:47 +0530

    Added : Use Case 5 Problem - Calculating wages for month of an Employee

--- src/com/employeewage/computation/EmployeeWageComputation.java
+++ src/com/employeewage/computation/EmployeeWageComputation.java
@@ -3,6 +3,7 @@
 //Use Case-2 is to calculate the daily Employee wage.
 //Use Case-3 is to show part-time Employee's wage.
 //Use Case-4 is to solve that previous program in switch case statement.
+//Use Case-5 is to calculate wages for a month of an Employee.
 
 package com.employeewage.computation;
 
@@ -22,34 +23,38 @@ public class EmployeeWageComputation {
         //Declaring the variables.
         final int isPartTime = 1;
         final int isFullTime = 2;
-        int randomCheck = (int) Math.floor((Math.random() * 10) % 3);
         int empHour;
         int wagePerHour = 20;
         int salary = 0;
+        int numWorkingDays = 20;
+        int monthlySalary = 0;
 
-        //Checking for part-time and full time by Switch case.
-        switch (randomCheck) {
-            default :
-                System.out.println("Employee is absent");
-                empHour=0;
-                salary = empHour * wagePerHour;
-                System.out.println("Salary is: $"+salary);
-                break;
-            case isFullTime :
-                System.out.println("Employee is working part-time");
-                int partTimeHour = 4;
-                empHour = partTimeHour;
-                salary = empHour * wagePerHour;
-                System.out.println("Salary is: $"+salary);
-                break;
-            case isPartTime :
-                System.out.println("Employee is working full-time");
-                int fullTimeHour = 8;
-                empHour = fullTimeHour;
-                salary = empHour * wagePerHour;
-                System.out.println("Salary is: $"+salary);
-                break;
+        //Calculating the number of working days of an Employee randomly.
+        for (int i=1; i <= numWorkingDays; i++)
+        {
+            int randomCheck = (int) Math.floor((Math.random() * 10) % 3);
+            switch (randomCheck) {
+                default :
+                    System.out.println("Employee is absent");
+                    empHour=0;
+                    salary = empHour * wagePerHour;
+                    break;
+                case isPartTime :
+                    System.out.println("Employee is working part-time");
+                    int partTimeHour = 4;
+                    empHour = partTimeHour;
+                    salary = empHour * wagePerHour;
+                    break;
+                case isFullTime :
+                    System.out.println("Employee is working full-time");
+                    int fullTimeHour = 8;
+                    empHour = fullTimeHour;
+                    salary = empHour * wagePerHour;
+                    break;
+            }
+            monthlySalary = monthlySalary + salary;
         }
+        System.out.println("Salary of an Employee in a month is : $"+monthlySalary);
 
     }
 }
