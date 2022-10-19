964ec32ebb1fc3950cace256e54efdc0a4ae904b
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-10-19 19:59:40 +0530

    Added : Display Welcome to Employee Wage Computation Program

---
+++ src/com/employeewage/computation/EmployeeWageComputation.java
@@ -0,0 +1,18 @@
+//This program is to display the welcome message to Employee wage computation program.
+
+package com.employeewage.computation;
+
+public class EmployeeWageComputation {
+
+    // Default Constructor
+    public EmployeeWageComputation() {
+        System.out.println("Welcome to Employee Wage Computation Program..!");
+    }
+
+    public static void main(String[] args) {
+
+        //Initializing the constructor by creating the object of the class.
+        EmployeeWageComputation employee = new EmployeeWageComputation();
+
+    }
+}
